f7e061fc79ee7682b4b05e04163eaaaad453304b
joe <qwert30830@gmail.com>
2017-03-29 18:35:43 +0800

    modify main.java

--- src/main/java/tw/teddysoft/bdd/domain/invoice/main.java
+++ src/main/java/tw/teddysoft/bdd/domain/invoice/main.java
@@ -10,34 +10,47 @@ public class main {
 
         public static void main(String[] args) {
 
-//            double vatRate = 0.0;
-//            int taxIncludedPrice = 0;
-//            int taxExcludedPrice=0;
-//            InvoiceBuilder _invoiceBuilder;
-//            for (int i = 0 ; i < 6 ; i++)
-//            {
-//                switch(args[i]) {
-//                    case "-i":
-//                        taxIncludedPrice = Integer.parseInt(args[i+1]);
-//                        break;
-//                    case "-r":
-//                         vatRate = Double.parseDouble(args[i+1]);
-//                        break;
-//                    case "-e":
-//                        taxExcludedPrice = Integer.parseInt(args[i+1]);
-//                        break;
-//                    default:
-//                        break;
-//                }
-//            }
-//            Invoice invoice = InvoiceBuilder.newInstance().
-//                    withVatRate(0.05).
-//                    withTaxIncludedPrice(123).
-//                    withTaxExcludedPrice(34286).
-//                    issue();
+            double vatRate = 0.0;
+            int taxIncludedPrice = 0;
+            int taxExcludedPrice=0;
+            InvoiceBuilder invoiceBuilder = InvoiceBuilder.newInstance();
 
+            System.out.println("請數入指令，-i 後面表示 含稅價 -r 表示稅率 -e 表示未稅價 (e.g : -i 100 -r 0.05 -e 101)");
 
-            System.out.println("hellohellohellohellohellohellohello");
+            for (int i = 0 ; i < args.length ; i++)
+            {
+                switch(args[i]) {
+                    case "-i":
+
+                        taxIncludedPrice = Integer.parseInt(args[i+1]);
+                        invoiceBuilder = invoiceBuilder.withTaxIncludedPrice(taxIncludedPrice);
+                        break;
+
+                    case "-r":
+
+                        vatRate = Double.parseDouble(args[i+1]);
+                        invoiceBuilder = invoiceBuilder.withVatRate(vatRate);
+                        break;
+
+                    case "-e":
+
+                        taxExcludedPrice = Integer.parseInt(args[i+1]);
+                        invoiceBuilder = invoiceBuilder.withTaxExcludedPrice(taxExcludedPrice);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            Invoice invoice = invoiceBuilder.issue();
+
+            System.out.println("含稅價格 = " + invoice.getTaxIncludedPrice());
+            System.out.println("未稅價格 = " + invoice.getTaxExcludedPrice());
+            System.out.println("稅金 = " + invoice.getVAT());
+            System.out.println("稅率 = " + invoice.getVatRate());
+
+//            System.out.println("hellohellohellohellohellohellohello");
 
 
         }
