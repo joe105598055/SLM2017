29d3f6b24bac639a5b93596bed419cfa60a4f36a
joe <qwert30830@gmail.com>
2017-03-29 18:13:48 +0800

    456

---
+++ src/main/java/tw/teddysoft/bdd/domain/invoice/main.java
@@ -0,0 +1,45 @@
+package tw.teddysoft.bdd.domain.invoice;
+
+import tw.teddysoft.bdd.domain.invoice.Invoice;
+import tw.teddysoft.bdd.domain.invoice.InvoiceBuilder;
+
+/**
+ * Created by joe on 2017/3/29.
+ */
+public class main {
+
+        public static void main(String[] args) {
+
+//            double vatRate = 0.0;
+//            int taxIncludedPrice = 0;
+//            int taxExcludedPrice=0;
+//            InvoiceBuilder _invoiceBuilder;
+//            for (int i = 0 ; i < 6 ; i++)
+//            {
+//                switch(args[i]) {
+//                    case "-i":
+//                        taxIncludedPrice = Integer.parseInt(args[i+1]);
+//                        break;
+//                    case "-r":
+//                         vatRate = Double.parseDouble(args[i+1]);
+//                        break;
+//                    case "-e":
+//                        taxExcludedPrice = Integer.parseInt(args[i+1]);
+//                        break;
+//                    default:
+//                        break;
+//                }
+//            }
+//            Invoice invoice = InvoiceBuilder.newInstance().
+//                    withVatRate(0.05).
+//                    withTaxIncludedPrice(123).
+//                    withTaxExcludedPrice(34286).
+//                    issue();
+
+
+            System.out.println("hellohellohellohellohellohellohello");
+
+
+        }
+}
+
